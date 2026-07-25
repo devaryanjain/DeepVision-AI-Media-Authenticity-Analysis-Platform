1bac24ee5c276a186422a701a979ff72f27593b7
devaryanjain <devaryan7704@gmail.com>
2026-07-25 22:16:00 +0530

    Add SHA-256 hashing service

--- backend/services/image_service.py
+++ backend/services/image_service.py
@@ -1,10 +1,13 @@
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-
 from ai_engine.predictor import predict
+from services.hash_service import generate_sha256
 
 
 def predict_image(image_path):
-    return predict(image_path)
+
+    prediction = predict(image_path)
+
+    file_hash = generate_sha256(image_path)
+
+    prediction["sha256"] = file_hash
+
+    return prediction
